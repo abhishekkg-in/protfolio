edcdf20f08929531fe1f5ccf856b6d5f04beedea
Abhishek Gupta <abhishek.61095728@ltimindtree.com>
2024-10-28 21:43:36 +0530

    initial commit of portfolio in plin html, css & javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll to section on link click
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
