d68d6183d0162ce66e17a94770e1ba2db015ad33
Abhishek Gupta <abhishek.61095728@ltimindtree.com>
2024-11-01 09:31:23 +0530

    updated about section and navbar

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 // Smooth scroll to section on link click
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth"
-      });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
   });
+});
+
+let lastScrollTop = 0;
+const navbar = document.getElementById("navbar");
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    navbar.style.top = "0";
+  } else if (currentScroll > lastScrollTop) {
+    navbar.style.top = "-60px";
+  } else {
+    navbar.style.top = "0";
+  }
+
+  lastScrollTop = currentScroll;
+});
+
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+};
+
+scrollToTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
